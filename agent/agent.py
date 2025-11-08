28cbecaa707e9212dd9517f35b4cddc8dc031530
Adriano Bonfiglio <adrianobonfiglio@Adrianos-MacBook-Air.local>
2025-11-08 11:30:28 -0500

    Split code and add adjust speech recoginition timeout and phrase limit

--- agent/agent.py
+++ agent/agent.py
@@ -1,65 +1,19 @@
 from langchain.agents import create_agent
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.tools import tool
-from langchain_community.utilities import GoogleSerperAPIWrapper
-from langchain_community.tools import DuckDuckGoSearchRun
-from google.cloud import texttospeech
-import pygame
 import json
-import subprocess
-from time import sleep
 import speech_recognition as sr
 from tool_management.todo import manage_todo_list
-from config import LLM_GEMINI_API_KEY, TTS_GEMINI_API_KEY, SERPER_API_KEY
+from config import LLM_GEMINI_API_KEY
 from urllib.parse import quote
 from tools import get_search_results, get_user_preference
+from speech import start_recognition, text_to_speech, audio_recognition
+from assitant_ui import open_assisent_page
 
 
 # Initialize the recognizer
 r = sr.Recognizer()
+
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", api_key=LLM_GEMINI_API_KEY)
-
-def text_to_speech(text:str, language:str="en-US"):
-    """Converts text to speech using Google Cloud Text-to-Speech API."""
-    client = texttospeech.TextToSpeechClient(client_options={"api_key": TTS_GEMINI_API_KEY})
-
-    synthesis_input = texttospeech.SynthesisInput(text=text)
-
-    voice = texttospeech.VoiceSelectionParams(
-        language_code=language,
-        model_name="gemini-2.5-pro-tts"
-
-    )
-    audio_config = texttospeech.AudioConfig(
-        audio_encoding=texttospeech.AudioEncoding.MP3,
-        speaking_rate=1.0,
-        pitch=0.0,
-    )
-
-    # Perform the text-to-speech request
-    response = client.synthesize_speech(
-        input=synthesis_input, voice=voice, audio_config=audio_config
-    )
-
-    # Save the audio content to a file
-    with open("output.mp3", "wb") as out:
-        out.write(response.audio_content)
-    play_audio("output.mp3")
-
-def play_audio(file_path: str):
-    """Plays an audio file using pygame."""
-    pygame.mixer.init()
-    pygame.mixer.music.load(file_path)
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy():
-        pygame.time.Clock().tick(10)
-
-def recognize_audio(source, recognizer):
-    """Listens for the wake word 'Hey Jarvis' and triggers the assistant."""
-    text = audio_recognition(source, recognizer)
-    print(f"You said: {text}")
-    return text
-
 
 def listen_to_user():
     with sr.Microphone() as source:
@@ -70,47 +24,23 @@ def listen_to_user():
         while True:
             # Listen for the user's input
             print("Listening...")
-            text = recognize_audio(source, r)
+            text = audio_recognition(source, r)
             if(text is not None and text.lower().strip().startswith("jarvis")):
                 text = text.lower().strip()
                 open_assisent_page("loader")
                 print("Listening sequence...")
-                text = recognize_audio(source, r)
-                text = text.lower().strip()
-                if(text == "home"):
+                text = start_recognition(source, r)
+                if(text is not None and text == "home"):
                     open_assisent_page("home")
 
-                elif(text.startswith("play")):
+                elif(text is not None and text.lower().strip().startswith("play")):
                     text = text.replace("play", "")
                     text = quote(text)
                     open_assisent_page("youtube-music", text)
 
-                else:
+                elif(text is not None):
                     print("calling agent...")
                     execute_agent(text)
-
-def open_assisent_page(page_name: str, params: str = None):
-    command = f"open assistant://{page_name}"
-    if params:
-        command += f"/param={params}"
-    print(f"--- Opening assistant page with command: {command} ---")
-    subprocess.call(command, shell=True)
-
-
-def audio_recognition(source, recognizer,timeout=4, phrase_time_limit=4):
-    try:
-        audio = r.listen(source, timeout, phrase_time_limit)
-        return recognizer.recognize_google(audio)   
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio.")
-    except sr.RequestError as e:
-        print(f"Could not request results from Google Speech Recognition service; {e}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-def open_agent_ear(source, recognizer):
-    text = audio_recognition(source, recognizer, 10, 10)
-    execute_agent(text)
 
 agent = create_agent(
     model=llm,
